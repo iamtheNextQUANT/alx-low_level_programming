c7c51eb2ec40e150b92c827c9fe585ea73d73888
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-12 08:35:48 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	i =48;
+	j =48;
+	while (i< 58)
+	{
+		j =48;
+		while (j <58)
+		{
+			putchar(i);
+			putchar(j);
+			if (i <57 || j<57)
+			{
+				putchar (44);
+				putchar (32);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+	return(0);
+
+}
