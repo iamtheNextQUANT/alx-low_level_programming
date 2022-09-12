654add05b7fb2d10b4f836526e80b4c7f10aa4f2
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-12 10:16:24 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,26 +1,27 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-	int i,j,k,m;
-	i =48;
-	while (i<58)
+	int i, j, k, m;
+
+	i = 48;
+	while (i <58)
 	{
-		j =48;
-		while (j <58)
+		j = 48;
+		while (j < 58)
 		{
-			m =j +1;
-			k =1;
-			while(k <58)
+			m =j+1;
+			k =i;
+			while (k <58)
 			{
-				while (m <58)
+				while (m<58)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(32);
 					putchar(k);
 					putchar(m);
-					if (i<57 || j<56 || k<57 ||m<57)
+					if (i <57 || j<56 || k<57 ||m<57)
 					{
 						putchar(44);
 						putchar(32);
@@ -28,14 +29,13 @@ int main(void)
 					}
 					m++;
 				}
-				m=48;
+				m =48;
 				k++;
 			}
 			j++;
-
 		}
 		i++;
 	}
 	putchar(10);
-	return(0)
+	return(0);
 }
