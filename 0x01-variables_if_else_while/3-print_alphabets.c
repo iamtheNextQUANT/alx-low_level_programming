8b1cd91a704c94af9a1d605db9716e2b6877bfeb
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-12 02:35:54 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i,j;
+	i = 97;
+	j = 65;
+
+	while (i <123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 91)
+	{
+		putcahr(j);
+		j++;
+	}
+
+	putchar(10);
+	return(0);
+
+}
