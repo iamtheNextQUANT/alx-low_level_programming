f191afe23abf3249bb6c179cbf277c506186e640
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-13 05:09:08 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10
+ */
+
+
+void print_alphabet_x10(void)
+{
+	int circle = 0;
+	char c;
+
+	while (i < 10)
+
+	{
+		c = "a";
+
+		while (c <= "z")
+		{
+			_putchar(c);
+			c++;
+
+		}
+
+		_putchar('\n')
+
+		i++;
+	}
+}
