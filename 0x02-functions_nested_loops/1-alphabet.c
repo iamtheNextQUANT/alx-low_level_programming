918286636d635fe27515605ab94a70d8ed54b413
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-13 04:28:21 -0700

    fund

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,24 @@
 #include "main.h"
 
 
+ 
 void print_alphabet(void)
+
 {
+
 	char letter = 'a';
+
 	while (letter <= 'z')
+
 	{
 		_putchar(letter);
+
 		letter++;
+
 	}
+
 	_putchar('\n');
+
 }
+
+
