d770af1a861e14a90e5ee19a010b07404433b38b
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-13 09:31:36 -0700

    aheoh

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/*
+ * snumb
+ * er_sign
+ */
+
+int print_sign(int n)
+{
+	if (n >0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	else if(n 0)
+	{
+		_putchar('-');
+
+
+
+		return(-1);
+	}
+	else
+	{
+		_putchar('0')
+		return(0);
+	}
+}
