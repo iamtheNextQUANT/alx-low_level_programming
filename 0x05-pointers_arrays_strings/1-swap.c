a230fca4a7dfec08ee0b5525c030f0cd90cb794d
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-19 13:40:19 -0700

    tsk1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap int _of to input numbers
+ * @a: input values a
+ * @b: input values b
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a
+	*a = *b
+	*b = tmp
+
+}
