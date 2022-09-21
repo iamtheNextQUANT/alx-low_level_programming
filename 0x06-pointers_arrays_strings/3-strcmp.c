b794cfeaab8002f16bb264498f7ee383b77d9453
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-21 10:31:08 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+ * strcmp ->for comparing purpose
+ * @s1: parameter1
+ * @s2: parameter2
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2);
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+
+}
+
