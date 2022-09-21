dbb37e2bba1a848d4d9b9524419456ff21345687
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-21 10:17:14 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+		int i;
+			int len = 0;
+
+				while (dest[len] != '\0')
+						{
+									len++;
+										}
+
+					for (i = 0 ; i < n && src[i] != '\0' ; i++)
+								dest[len + i] = src[i];
+						dest[len + i] = '\0';
+
+							return (dest);
+}
+
