a4015c4b1d230e7fe96a9f4ae40351a5057a8bf1
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-22 00:50:21 -0700

    task1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include  <stdio.h>
+
+/**
+ * main -infinite loop
+ * Return 0
+ */
+
+int main(void)
+{
+	int i;
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+
+	printf("infinite loop avoide! \\0/\n");
+	return(0);
+
+}
