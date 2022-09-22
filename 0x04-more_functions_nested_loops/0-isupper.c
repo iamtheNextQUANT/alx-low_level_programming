68e76c4ce0c5b4b8e1f7c33ba235f79eb028e9d4
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-22 04:26:21 -0700

    yeoh

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
+
+#include "holberton.h"
 /**
- * @c: input
- * _isupper: uppercase check
- * Return 0 
- */
-
+ *  * _isupper - check the code for Holberton School students.
+ *   * @c: Character input
+ *    * Return: Always 0.
+ *     */
 int _isupper(int c)
-
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		
-		return (0);
-
+		if (c >= 'A' && c <= 'Z')
+					return (1);
+			else
+						return (0);
 }
