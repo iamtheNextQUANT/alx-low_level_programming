608eb47c45b3ace7e060b4d3f169a9f9982b3bd2
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-22 11:39:06 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
+	char letter[] = "_putchar";
+	int i;
 
 	for (i = 0; i < 9; i++)
 	{
-		_putchar(text[i]);
+		_putchar(letter[i]);
 	}
+
 	_putchar('\n');
 
 	return (0);
-
 }
