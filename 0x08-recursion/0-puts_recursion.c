603cc139e00e527f6b97bc2bbfa8fb26cebe492d
iamtheNextQUANT <dukeobare99@gmail.com>
2022-09-28 18:47:37 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
+  * _print_rev_recursion - check the code for Holberton School students.
+  *  * @s: Inputting string
+  *   * Return: Always 0.
  *     */
 void _puts_recursion(char *s)
 {
-if (*s != '\0')
-		{
-					_putchar(*(s + 0));
-							_puts_recursion((s + 1));
-								}
+	if(*s == '\0')
+	{
+		_putchar('\n');
+	}
 	else
-			{
-						_putchar('\n');
-								return;
-									}
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+
+	}
 }
