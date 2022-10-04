fcfbd73cd3de56ffa873586ebfd2ef254cf36392
iamtheNextQUANT <dukeobare99@gmail.com>
2022-10-04 14:10:24 -0700

    task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,7 +10,7 @@
 char *argsostr(int ac, char **av){                                                                                  
 	        char *str;                                                                                                  
 		        int count =0, a =0, b =0, c=0;                                                                              
-			        if (ac ==0 || av == NULL)                                                                                    
+			        if (ac ==0 || av == NULL)                                                                                   
 					                return(NULL);                                                                                       
 				        while (a < ac){                                                                                             
 						                b=0;                                                                                                
@@ -28,16 +28,15 @@ char *argsostr(int ac, char **av){
 									        {                                                                                                           
 											                return (NULL);                                                                                      
 													        }                                                                                                           
-								        for (a =0; a < ac; a++)                                                                                     
-										                for (b =0; av[a][b] != '\0'; b++){                                                                   
+								        for (a =0; a < ac; a++){                                                                                    
+										                for (b =0; av[a][b] != '\0'; b++){                                                                  
 													                {                                                                                                   
 																                        str[c] = av[a][b];                                                                          
 																			                        c++;                                                                                        
 																						                }                                                                                                   
-									                str[c] = '\0';                                                                                      
-											                c++;                                                                                                
-													        }                                                                                                           
-        return (str);                                                                                               
-	                                                                                                                    
-	}                                                                                                                   
-     
+															                str[c] = '\0';                                                                                      
+																	                c++;                                                                                                
+																			        }                                                                                                           
+												        return (str);                                                                                               
+													                                                                                                                    
+									} 
