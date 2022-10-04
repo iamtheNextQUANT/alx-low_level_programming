4a8acfc21d139838788e8e535269c19326352ed8
iamtheNextQUANT <dukeobare99@gmail.com>
2022-10-04 06:46:03 -0700

    all

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,35 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mem_clear(char **s)
-{
-		/* Loop through and free up each row of memory, then free up the block itself */
-}
-
 /**
- *  * word_count - Counts number of indexes in a string
- *   * @str: String being counted
- *    * Return: Number of indexes
- *     */
-int word_count(char *str)
+ *  * simple_print_buffer - prints buffer in hexa
+ *   * @buffer: the address of memory to print
+ *    * @size: the size of the memory to print
+ *     *
+ *      * Return: Nothing.
+ *       */
+char *create_array(unsigned int size, char c)
 {
-		/* Loop through given string */
-			/* If the current index is anything other than a space, increment counter by 1 */
-				/* Check the next index(es) for either space or null byte */
-				/* If the end of the string is reached, exclude the null byte itself by decrementing by 1 */
-		/* Return the word count*/
-}
+	  unsigned int i;
 
-/**
- *  * strtow - "Master function": Splits a string such that each word gets its own line
- *   * @str: String input
- *    * Return: The pointer to the very first index of the list of words
- *     */
-char **strtow(char *str)
-{
-int i, j = 0;
-	int input_length, word_size, array_row = 0, array_column;
-		char **new_str = NULL;
-}
-	/* Return our newly split up 2d array of strings */
+	    if (size == 0)
+		          return (NULL);
+
+	      char *s = (char*)malloc(size * sizeof(char));
+
+	        for (i = 0; i < size; i++)
+			    s[i] = c;
+
+		  return (s);
 }
